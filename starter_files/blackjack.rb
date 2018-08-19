0a8b730d47cec932a2c80a1eea5d81d548ac1171
lstravers <stravers222@gmail.com>
2018-08-19 18:36:15 -0400

    No blackjack code to add. Working w/ the Learn Ruby the Hard Way document. Completed exercises 1-12.  If ONLY I'd known to BEGIN with this last Monday! For absolute and complete beginnersthis resource should be provided in the Coding Foundations class.

--- starter_files/blackjack.rb
+++ starter_files/blackjack.rb
@@ -1,3 +1,11 @@
-puts "TODO Implement the game of blackjack."
+puts "TO DO: Implement the game of blackjack."
 
 # Hint: for starters, read bin/blackjack.rb
+
+puts "\nI have no blackjack game code to add. I am working with the 'Learn Ruby the Hard Way' .pdf document.
+
+\nI've completed exercises 1-12 and learned so much! Ruby is actually starting to make sense now!!
+
+\nI have the code files as proof but don't know how to upload them to my development branch.
+    
+\nI wish I had tried this link and discovered this document last Monday! I lost far too much time trying different websites to learn Ruby! If I had only known to BEGIN with this document!!!!!!!!!!!"
